04e879fcfe43b19ecc3e6bb453e3bf15217206d5
Артём Новиков <artemnovikov@MacBook-Pro-Artem.local>
2020-02-09 13:49:02 +0300

    [upd] xcode project

--- Native/VisualIPCV/DllExport.h
+++ Native/VisualIPCV/DllExport.h
@@ -1,9 +1,17 @@
 #pragma once
 
+#ifdef __APPLE__
+#ifndef IPCV_API
+#define IPCV_API
+#endif
+#else
+
 #ifndef IPCV_API
 #ifdef IPCV_EXPORT
 #define IPCV_API __declspec(dllexport)
 #else
 #define IPCV_API __declspec(dllimport)
 #endif
+#endif
+
 #endif
