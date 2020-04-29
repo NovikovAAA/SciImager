7a8657322b18f5913ebff4bf3f0dd368ee3e55c8
Артём Новиков <novickovaa32@gmail.com>
2020-04-29 21:00:13 +0300

    [upd] Загрузка плагинов

--- Native/VisualIpcvJava/com_visualipcv_ProcessorLibrary.h
+++ Native/VisualIpcvJava/com_visualipcv_ProcessorLibrary.h
@@ -2,5 +2,7 @@
 
 extern "C"
 {
+    JNIEXPORT void JNICALL Java_com_visualipcv_core_ProcessorLibrary_loadPlugins(JNIEnv* env, jclass clazz, jstring path);
+    JNIEXPORT void JNICALL Java_com_visualipcv_core_ProcessorLibrary_loadPluginsWithManualRegister(JNIEnv* env, jclass clazz, jstring path);
 	JNIEXPORT jobject JNICALL Java_com_visualipcv_core_ProcessorLibrary_getProcessorList(JNIEnv* env,  jclass clazz);
 }
